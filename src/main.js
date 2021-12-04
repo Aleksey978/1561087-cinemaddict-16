c28fc54b498e91b6fdb40d6be0c56f1573447736
Алексей Четвёркин <kexaxa4@gmail.com>
2021-12-04 14:22:46 +0900

    отрисовываем меню страницы

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {createSiteProfile} from './view/site-profile-view.js';
+import {createSiteMenu} from './view/site-menu-view.js';
 import {renderTemplate, RenderPosition} from './render.js';
 
 const siteHeaderElement = document.querySelector('.header');
+const siteMainElement = document.querySelector('.main');
 
 renderTemplate(siteHeaderElement, createSiteProfile(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteMenu(), RenderPosition.BEFOREEND);
